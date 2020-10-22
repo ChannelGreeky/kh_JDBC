a7c244adc34693f520bc149c20ad96807a9295fe
ChannelGreeky <dksk26@nate.com>
2020-10-22 17:57:57 +0900

    20201022n

---
+++ Setting/web_member.sql
@@ -0,0 +1,11 @@
+Create table delete_member as
+(select member_id, member_name, gender, phone, enroll_date
+from member where 1=0);
+
+alter table delete_member add
+constraint ID_PRI primary key(member_id);
+
+alter table delete_member add(END_DATE DATE);
+
+commit;
+
